42adf479d65a451a28264b0bf83e76dddb3458bc
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-22 15:19:43 +0700

    add multiuser login

---
+++ app/Exceptions/RoleNullException.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+
+class RoleNullException extends Exception
+{
+    //
+}
+
+class RoleNotFoundException extends Exception
+{
+    //
+}
+
--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -5,14 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use App\Models\SiswaAccountModel;
+use App\Models\AdminAccountModel;
+
+use Illuminate\Support\Facades\Cookie;
 
 use App\Enum as Enumlist;
-use App\Exception;
+use App\Exceptions\RoleNullException;
 
 
 class checkAuth {
     private Request $req;
     private string $message = "none";
+    private bool $status = false;
 
     // terima data dari req
     public function __construct(Request $req) {
@@ -21,42 +25,80 @@ class checkAuth {
 
     // req ke database
 
-    private function getDataFromModels($nomor_ujian) :SiswaAccountModel|null {
+    private function getDataFromModelsAsStudent($nomor_ujian): SiswaAccountModel|null {
         return SiswaAccountModel::where('nomor_ujian', $nomor_ujian)->first();
+    }
+
+    private function getDataFromModelsAsAdmin($username): AdminAccountModel|null {
+        return AdminAccountModel::where('username', $username)->first();
+    }
+
+    private function getDataByRole()
+    {
+        if ($this->req->has("role")) {
+            if ($this->req->get("role") == "student") {
+                return $this->getDataFromModelsAsStudent($this->req->get("identity"));
+            } else if ($this->req->get("role") == "admin") {
+                return $this->getDataFromModelsAsAdmin($this->req->get("identity"));
+            } else {
+                $this->message = "role tidak ditemukan!";
+                throw new RoleNotFoundException();
+            }
+        } else {
+            $this->message = "Mohon pilih sebagai apa anda login!";
+            throw new RoleNullException();
+        }
     }
 
     // cek password dari database
 
-    public function getPasswordFromObj($nomor_ujian)
+    public function getPasswordFromObj()
     {
-        $result = $this->getDataFromModels($nomor_ujian);
-        if ($result == null)
-        {
-            $this->message = "nomor ujian tidak ditemukan";
-            return 0;
-        } else {
-            return $result->password;
+        try{
+            $result = $this->getDataByRole();
+            if ($result != null) {
+                $this->Compare($this->req->get("password"), $result->password);
+            } else {
+                $this->message = "identitas tidak ditemukan";
+            }
+
+
+        } catch (RoleNullException | RoleNotFoundException) {
+            $this->status = false;
         }
+        
+        // if ($result == null)
+        // {
+        //     $this->message = "nomor ujian tidak ditemukan";
+        //     return 0;
+        // } else {
+        //     return $result->password;
+        // }
         
     }
 
-
-
-    private function Compare(): bool
+    private function Compare($password_from_user, $password_from_db): void
     {
-        $res = password_verify($this->req->get("password"), $this->getPasswordFromObj($this->req->get("nomor_ujian")));
+        $res = password_verify($password_from_user, $password_from_db);
         if ($res == false) {
             $this->message = "password salah";
+         
         } else {
             $this->message = "login berhasil";
+            $this->status = true;
         }
-        return $res;
     }
 
     public function GetResult()
     {
+        $this->getPasswordFromObj();
+
+        // return
+        // status = true or false, true for correct password
+        // messange = custom messange for server
+        // login
         return [
-            "status" => $this->Compare(),
+            "status" => $this->status,
             "message" => $this->message
         ];
     }
@@ -68,7 +110,7 @@ class LoginController extends Controller
     private $message;
     public function validateRequest($request): bool
     {
-        if($request->has('nomor_ujian') && $request->has('password'))
+        if($request->has('identity') && $request->has('password'))
         {
             $this->message = "valid_request";
             return 1;
@@ -92,13 +134,16 @@ class LoginController extends Controller
     public function login(Request $request)
     {
         // set cookie to user
-        $cookie = cookie('login_status', $this->GetLoginStatus($request), 600);
+        // $cookie = 
+        // Cookie::queue(cookie('login_status', "oke", 600));
 
         // send cookie to user
         return response([
             "status" => $this->GetLoginStatus($request),
             "message" => $this->message
-        ])->cookie($cookie);
+
+        ])->cookie(cookie('login_status', $this->GetLoginStatus($request), 600))
+            ->cookie(cookie('debugtwo', true, 600));
         
     }
 }
