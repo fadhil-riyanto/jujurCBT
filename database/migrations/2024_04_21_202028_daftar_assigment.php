e80cd1ae0480943d136fe5ae5b97fd004c784f69
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-22 14:57:00 +0700

    add assignment database each student

---
+++ database/migrations/2024_04_21_202028_daftar_assigment.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("daftar_assignment", function(Blueprint $table) {
+            $table->id();
+            $table->integer("nomor_ujian");
+            $table->string("jenis_mapel");
+            $table->boolean("status_dikerjakan");
+            $table->boolean("status_selesai");
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists("daftar_assignment");
+    }
+};
