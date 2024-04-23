fc6df70f26b7d05738329869cfb6a3caa49739db
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-23 11:14:18 +0700

    test error page

--- app/Http/Controllers/CookiedebugController.php
+++ app/Http/Controllers/CookiedebugController.php
@@ -3,11 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits;
 
 class CookiedebugController extends Controller
 {
+    use Traits\CurrentSessionTrait;
     //
     public function debug(Request $request) {
-        return dd($request->cookie("debugtwo") === true);
+        // return dd($request->cookie("debugtwo") === true);
+        // dd(unserialize($request->cookie("login_data")));
+        // return response("okok")->cookie(cookie("keys",  serialize([
+        //     "key1" => "val1",
+        //     "key2" => "val2"
+        // ])));
+
+        $this->cookie_deserialize($request);
+        // dd($this);
+        return view("error.illegalroute");
     }
 }
