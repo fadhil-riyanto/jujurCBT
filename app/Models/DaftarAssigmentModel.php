901ed48a2208e20aa09e9fc8a7f617714f7147ee
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-23 11:13:18 +0700

    add daftar assigment models

---
+++ app/Models/DaftarAssigmentModel.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class DaftarAssigmentModel extends Model
+{
+    use HasFactory;
+    protected $table = "daftar_assignment";
+    protected $primaryKey = "id";
+    protected $fillable = ["nomor_ujian", "jenis_mapel", "status_dikerjakan", "status_selesai"]
+}
