97eab3de267a0bbcb8f15fcc53809c3f91b80de7
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-23 11:31:27 +0700

    add comment for better look

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,11 @@ use App\Http;
 use App\Http\Middleware;
 
 Route::prefix("/api")->group(function() {
+    // global
     Route::post("/auth", [Http\Controllers\LoginController::class, "login"]);
+    
 
+    // only for student
     Route::prefix("/dashboard")->group(function() {
         Route::get("/get_mata_pelajaran", [Http\Controllers\GetMataPelajaranController::class, "GetData"]);
         Route::post("/get_me", [Http\Controllers\GetMeController::class, "getData"]);
@@ -14,20 +17,21 @@ Route::prefix("/api")->group(function() {
     
 });
 
-Route::get('/admin', function () {
-    return view("views/admin");
-});
-
-Route::get('/login', function () {
-    return view("views/login");
-});
-
-
+/* debug route */
 Route::get('/debug', [Http\Controllers\CookiedebugController::class, "debug"]);
 
+/* route for web */
+
+Route::get('/admin', function () {
+    return view("views/admin");
+})->middleware(Middleware\EnsureUsersOnAdmin::class);
 
 Route::get('/dashboard', function () {
     return view("views/dashboard");
 })->middleware(Middleware\EnsureUsersOnStudent::class);
+
+Route::get('/login', function () {
+    return view("views/login");
+});
 
 Route::get('/', [Http\Controllers\IndexController::class, "IndexController"]);
