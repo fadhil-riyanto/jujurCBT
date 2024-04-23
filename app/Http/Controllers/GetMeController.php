8dfcfe416672a8af334c0d32e362476e2dca781a
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-23 11:13:49 +0700

    finalizing endpoint /get_me

--- app/Http/Controllers/GetMeController.php
+++ app/Http/Controllers/GetMeController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Traits;
+use App\Models;
+use App\Enum;
 
 class GetMeController extends Controller
 {
-    public function getData()
-    {
+    use Traits\CurrentSessionTrait;
+    private function returnNameAsRole():string {
+        $cb = match($this->cookie_role) {
+            Enum\RoleSessionEnum::Student => Models\SiswaAccountModel::where("nomor_ujian", $this->cookie_identity)->first(),
+            Enum\RoleSessionEnum::Admin => Models\AdminAccountModel::where("username", $this->cookie_identity)->first()
+        };
+
+        return $cb->nama;
         
+    }
+    public function getData(Request $req)
+    {
+        $this->cookie_deserialize($req);
+        
+        return response([
+            "data" => $this->returnNameAsRole()
+        ]);
     }
 }
