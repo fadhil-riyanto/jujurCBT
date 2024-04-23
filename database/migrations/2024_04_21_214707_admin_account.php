498ec6e7c6f799f617d7f7545728fe69f7fd4248
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-23 10:31:54 +0700

    add nama database properties

--- database/migrations/2024_04_21_214707_admin_account.php
+++ database/migrations/2024_04_21_214707_admin_account.php
@@ -14,6 +14,7 @@ return new class extends Migration
         //
         Schema::create("admin_account", function(Blueprint $table) {
             $table->id();
+            $table->string("nama");
             $table->string("username");
             $table->string("password");
         });
@@ -24,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Shema::dropIfExists("admin_account");
+        Schema::dropIfExists("admin_account");
     }
 };
