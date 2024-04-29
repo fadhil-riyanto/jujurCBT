8411278572cba4e3d87177c1263b61c8d950e22f
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-29 20:49:45 +0700

    add models to list all assigments

--- app/Models/DaftarAssigmentModel.php
+++ app/Models/DaftarAssigmentModel.php
@@ -10,5 +10,5 @@ class DaftarAssigmentModel extends Model
     use HasFactory;
     protected $table = "daftar_assignment";
     protected $primaryKey = "id";
-    protected $fillable = ["nomor_ujian", "jenis_mapel", "status_dikerjakan", "status_selesai"]
+    protected $fillable = ["nomor_ujian", "jenis_mapel", "status_dikerjakan", "status_selesai"];
 }
