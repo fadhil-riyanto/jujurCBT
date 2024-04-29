7d45d34a7fa485de678d5628fc1553da4afebcfb
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-04-29 20:48:03 +0700

    replace old models eloquent with repository pattern

--- app/Http/Controllers/GetMeController.php
+++ app/Http/Controllers/GetMeController.php
@@ -4,23 +4,36 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Traits;
-use App\Models;
 use App\Enum;
+use App\Repositories;
+use App\Exceptions;
 
 class GetMeController extends Controller
 {
     protected Request $request;
-
     use Traits\CurrentSessionTrait;
-    private function returnNameAsRole():string {
-        $cb = match($this->cookie_role) {
-            Enum\RoleSessionEnum::Student => Models\SiswaAccountModel::where("nomor_ujian", $this->cookie_identity)->first(),
-            Enum\RoleSessionEnum::Admin => Models\AdminAccountModel::where("username", $this->cookie_identity)->first()
-        };
+    
+    public function __construct(
+        protected Repositories\SiswaAccountRepository $siswa_account_db, 
+        protected Repositories\AdminAccountRepository $admin_account_db, 
+    ) {}
 
-        return $cb->nama;
+    private function returnNameAsRole(): string {
+        try {
+            $cb = match($this->cookie_role) {
+                Enum\RoleSessionEnum::Student => 
+                    $this->siswa_account_db->getByTable("nomor_ujian", $this->cookie_identity),
+                Enum\RoleSessionEnum::Admin => 
+                    $this->admin_account_db->getByTable("username", $this->cookie_identity)
+            };
+            return $cb->getFirst()->nama;
+        } catch (Exceptions\DataNotFoundByModel) {
+            return "error";
+        }
+        
         
     }
+
     public function getData(Request $request)
     {
         $this->request = $request;
