0485a001c36687108eef90e9d3d83fabdbbc9fef
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-05-15 07:53:03 +0700

    add new helper func to convert char into arr offset

--- app/Helpers/stringHelper.php
+++ app/Helpers/stringHelper.php
@@ -11,3 +11,17 @@ if (!function_exists("normal2snake_case_invers")) {
         return strtoupper(str_replace("_", " ", $not_normal));
     }
 }
+
+if (!function_exists("literal2charindex")) {
+    function literal2charindex(string $literal): int {
+        $char_literals = [
+            "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+            "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+        ];
+        for($i = 0; $i < count($char_literals); $i++) {
+            if (strtoupper($literal) == $char_literals[$i]) {
+                return $i;
+            }
+        }
+    }
+}
