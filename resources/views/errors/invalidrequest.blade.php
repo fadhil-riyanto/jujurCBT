6b5f6bd33efaff02b1475738ec73e07d99a5cf94
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-05-23 09:00:54 +0700

    add frontend exception for invalidrequest

---
+++ resources/views/errors/invalidrequest.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <title>jujurCBT</title>
+
+        @vite("resources/css/error.css")
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="assets/js/utils.js"></script>
+    </head>
+
+    <body>
+        
+
+        <div class="container">
+            <h1>Waduh</h1>
+            <hr>
+            <p class="error-reason">Kamu sepertinya mencoba mengakses sesuatu yang bukan milikmu melalui link URL :(</p>
+            <p class="error-additional-info">jika menurutmu ini kesalahan, silahkan kontak administrator</p>
+            <a href="/" class="back">kembali ke awal</a>
+        </div>
+
+    </body>
+</html>
