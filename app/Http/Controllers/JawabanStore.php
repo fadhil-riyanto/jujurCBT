b287185851b2000ed640d77638c3be5252debca4
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-05-25 16:51:44 +0700

    add controller for storing ajax jawaban

---
+++ app/Http/Controllers/JawabanStore.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories;
+use Illuminate\Http\Request;
+
+class JawabanStore extends Controller
+{
+    public function __construct(
+        protected Repositories\onRunTimePilihanGandaRepository $on_runtime_pg_repo,
+        protected Repositories\onRunTimeEssayRepository $on_runtime_essay_repo
+        
+        // 
+    ){}
+
+    public function store_pilihan_ganda(Request $request) {
+        $validated = $request->validate([         // convention list
+            "kode_mapel" => "required",           // mata_pelajaran
+            "nomor_ujian" => "required",          // nomor_ujian
+            "id_soal" => "required",              // id_soal
+            "id_jawaban" => "required"            // index_jawaban
+        ]);
+
+        $this->on_runtime_pg_repo->insert_siswa_selection(
+            $request->kode_mapel, 
+            $request->nomor_ujian,
+            $request->id_soal,
+            $request->id_jawaban
+        );
+    }
+
+    // note is correct itu sebagai realtime data bagi akun pengampu
+    // is fixed diubah jadi true semua kalau siswa sudah menyelesaikan ujian / waktu habis
+    
+    // hapus column state
+
+    public function store_essay(Request $request) {
+        $validated = $request->validate([         // convention list
+            "kode_mapel" => "required",           // mata_pelajaran
+            "nomor_ujian" => "required",          // nomor_ujian
+            "id_soal" => "required",              // id_soal
+            "jawaban_txt" => "required"            // index_jawaban
+        ]);
+
+        $this->on_runtime_essay_repo->insert_siswa_essay(
+            $request->kode_mapel, 
+            $request->nomor_ujian,
+            $request->id_soal,
+            $request->jawaban_txt
+        );
+    }
+
+    // hapus state column
+    // rename index_jawaban ke id_soal
+    // ubah jawaban _txt ke text()
+}
