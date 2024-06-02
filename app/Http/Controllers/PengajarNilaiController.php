b1be9de294d37352aff4f928a80f8f1e992a65e1
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-02 18:35:49 +0700

    add new handler for /koreksi_essay

--- app/Http/Controllers/PengajarNilaiController.php
+++ app/Http/Controllers/PengajarNilaiController.php
@@ -58,13 +58,25 @@ class PengajarNilaiController extends Controller
         // return $pack;
     }
 
-    public function Index(Request $request) {
+    public function IndexNilai(Request $request) {
         $this->request = $request;
         $this->cookie_deserialize();
 
         $data2ret = iterator_to_array($this->get_penugasan_by_kode_mapel_pengampu());
         // dd($data2ret);
         return view('views/pengajar_nilai', [
+            "data" => $data2ret
+        ]);
+
+    }
+
+    public function IndexKoreksiEssay(Request $request) {
+        $this->request = $request;
+        $this->cookie_deserialize();
+
+        $data2ret = iterator_to_array($this->get_penugasan_by_kode_mapel_pengampu());
+        // dd($data2ret);
+        return view('views/pengajar_koreksi_essay', [
             "data" => $data2ret
         ]);
 
