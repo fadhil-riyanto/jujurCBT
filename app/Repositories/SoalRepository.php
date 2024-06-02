9ce22923239746e12327335e30ba142fa1155756
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-02 23:27:38 +0700

    add new functions, get ordered list id for essay and pilgan

--- app/Repositories/SoalRepository.php
+++ app/Repositories/SoalRepository.php
@@ -54,6 +54,20 @@ class SoalRepository {
             ->get();
     }
 
+    public function get_ordered_list_id_pilgan($kode_mapel) {
+        return $this->soal_db->where("mata_pelajaran", "=", $kode_mapel)
+            ->where("tipe_soal", "=", "pilihan_ganda")
+            ->select("id")
+            ->get();
+    }
+
+    public function get_ordered_list_id_essay($kode_mapel) {
+        return $this->soal_db->where("mata_pelajaran", "=", $kode_mapel)
+            ->where("tipe_soal", "=", "essay")
+            ->select("id")
+            ->get();
+    }
+
     public function hasEssay($kode_mapel) : int {
         return $this->soal_db->where("mata_pelajaran", "=", $kode_mapel)
             ->select("tipe_soal")
