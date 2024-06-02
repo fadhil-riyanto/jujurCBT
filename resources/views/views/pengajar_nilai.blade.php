947e6f3f2010bffe9a87b358c6164537049cf677
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-02 18:34:42 +0700

    add details

--- resources/views/views/pengajar_nilai.blade.php
+++ resources/views/views/pengajar_nilai.blade.php
@@ -7,10 +7,10 @@
         <div class="card">
             <h5 class="card-header">{{ normal2snake_case_invers($data_s["kelas_id"]) }}</h5>
             <div class="card-body">
-              <h5 class="card-title">{{ $data_s["nama_mapel"] }} (ujian tanggal {{ $data_s["start_date"] }})</h5>
-              <p class="card-text">mulai: {{ $data_s["start_time"] }}</p>
-              <p class="card-text">berakhir: {{ add_unix_mins_return_format($data_s["unix"], $data_s["duration_time"], "H:i") }}</p>
-              <a href="/pengajar/nilai/check?kelas={{ $data_s['kelas_id'] }}&penugasan_id={{ $data_s['id'] }}&kode_mapel={{ $data_s['kode_mapel'] }}" class="btn btn-primary">Lihat nilai</a>
+                <h5 class="card-title">{{ $data_s["nama_mapel"] }} (ujian tanggal {{ $data_s["start_date"] }})</h5>
+                <p class="card-text">mulai: {{ $data_s["start_time"] }}</p>
+                <p class="card-text">berakhir: {{ add_unix_mins_return_format($data_s["unix"], $data_s["duration_time"], "H:i") }}</p>
+                <a href="/pengajar/nilai/check?kelas={{ $data_s['kelas_id'] }}&penugasan_id={{ $data_s['id'] }}&kode_mapel={{ $data_s['kode_mapel'] }}" class="btn btn-primary">Lihat nilai</a>
             </div>
           </div>
     </div>
@@ -20,4 +20,7 @@
 @endsection
 
 @section("script")
+<script>
+    sidebar_change_state("#sidebar-pengajar-nilai")
+</script>
 @endsection
