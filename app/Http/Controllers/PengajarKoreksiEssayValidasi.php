45671a3bcfad1093b76e68fd2c225b5a641a4c6f
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-02 18:32:26 +0700

    feat: add table validation list

---
+++ app/Http/Controllers/PengajarKoreksiEssayValidasi.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Repositories;
+
+class PengajarKoreksiEssayValidasi extends Controller
+{
+    public function __construct(
+        protected Repositories\SiswaAccountRepository $siswa_repo,
+
+        // protected Repositories\PenyelesaianRepository $penyelesaian_repo,
+        // protected Repositories\SoalRepository $soal_repo,
+        // protected Repositories\PgRepo $pg_repo,
+
+        // protected Repositories\onRunTimePilihanGandaRepository $on_runtime_pilgan,
+        protected Repositories\onRunTimeEssayRepository $on_runtime_essay,
+        protected Repositories\PenyelesaianRepository $penyelesaian_repo
+        
+        
+        // 
+    ) {}
+
+    private function is_done($nomor_ujian) {
+        return $this->penyelesaian_repo->set($this->kode_mapel, $nomor_ujian, $this->penugasan_id)
+            ->isFixed();
+    }
+
+    /**
+     * logic: if all record points not null, its done
+     */
+    private function return_correction_status($nomor_ujian) { 
+        if ($this->is_done($nomor_ujian)) {
+            $datafrom_student_answer = $this->on_runtime_essay->get_all_answer_by_siswa_and_penugasan(
+                $nomor_ujian, $this->kode_mapel, $this->penugasan_id
+            );
+
+            if (count($datafrom_student_answer) == 0) {
+                return "-";
+            } else {
+                foreach($datafrom_student_answer as $datafrom_answer_s) {
+                    if ($datafrom_answer_s["points"] == null) {
+                        return "belum"; // not corrected yet
+                    }
+                }
+            }
+            
+            return "ya";
+        } else {
+            return "belum selesai";
+        }
+        
+    }
+
+    private function return_status($nomor_ujian) { 
+        if ($this->is_done($nomor_ujian)) {
+            $datafrom_student_answer = $this->on_runtime_essay->get_all_answer_by_siswa_and_penugasan(
+                $nomor_ujian, $this->kode_mapel, $this->penugasan_id
+            );
+            
+            if (count($datafrom_student_answer) == 0) {
+                return "selesai tapi tidak mengerjakan essay sama sekali";
+            } 
+            
+            return "selesai mengerjakan";
+        } else {
+            return "belum mengerjakan";
+        }
+        
+    }
+
+    private function return_siswa_by_spesific_class() {
+        return $this->siswa_repo->getByTable("kelas", $this->kelas)->get();
+    }
+
+    private function generate_aksi_btn_state($nomor_ujian) { // return true to disable
+        if (!$this->is_done($nomor_ujian)) { // not done yet
+            return true;
+        } else {
+
+        }
+    }
+
+    private function pack_data(): Iterable {
+        
+        foreach($this->return_siswa_by_spesific_class() as $data_s) {
+            // $this->return_correction_status($data_s["nomor_ujian"]);
+            if ($this->is_done($data_s["nomor_ujian"])) {
+                yield [
+                    "nama" => $data_s["nama"],
+                    "terkoreksi" => $this->return_correction_status($data_s["nomor_ujian"]),
+                    "status" => $this->return_status($data_s["nomor_ujian"]),
+                    "aksi" => $this->return_correction_status($data_s["nomor_ujian"]),
+                    "nomor_ujian" => $data_s["nomor_ujian"]
+                ];
+            }
+            
+        }
+    }
+
+    public function Index(Request $request) {
+        $validated = $request->validate([         // convention list
+            "kelas" => "required",
+            "kode_mapel" => "required",
+            "penugasan_id" => "required"
+        ]);
+
+        $this->kelas = $request->kelas;
+        $this->penugasan_id = $request->penugasan_id;
+        $this->kode_mapel = $request->kode_mapel;
+
+        // dd($this->return_siswa_by_spesific_class());
+
+        return view("views/pengajar_koreksi_essay_validasi", [
+            "data" => iterator_to_array($this->pack_data()),
+            "preload_data" => [
+                "kelas" => $this->kelas,
+                "penugasan_id" => $this->penugasan_id,
+                "kode_mapel" => $this->kode_mapel
+            ]
+        ]);
+    }
+}
---
+++ resources/views/views/pengajar_koreksi_essay_validasi.blade.php
@@ -0,0 +1,45 @@
+@extends("layout._layout_pengajar")
+
+@section("content") 
+<div class="alert alert-primary mt-1" role="alert">
+    Catatan: data yang ditampilkan disini hanya data bagi mereka yang sudah selesai mengerjakan ujian. 
+    jika siswa ybs belum selesai, maka ia tidak akan muncul disini
+</div>
+  
+<table id="nilai_siswa" class="display" style="width:100%">
+    <thead>
+        <tr>
+            <th>Nama</th>
+            <th>status</th>
+            <th>terkoreksi?</th>
+            <th>aksi</th>
+            
+        </tr>
+    </thead>
+    <tbody>
+        @foreach ($data as $data_s)
+        <tr>
+            <th>{{ $data_s["nama"] }}</th>
+            <th>{{ $data_s["status"] }}</th> 
+            <th>{{ $data_s["terkoreksi"] }}</th> 
+            @if ($data_s["aksi"] == "ya")
+                <th><a type="button" class="btn btn-secondary">koreksi lagi</a></th> 
+            @elseif ($data_s["aksi"] == "belum") 
+            <th><a type="button" class="btn btn-secondary" href="/pengajar/koreksi_essay/validasi/check?kelas={{ $preload_data['kelas'] }}&penugasan_id={{ $preload_data['penugasan_id'] }}&kode_mapel={{ $preload_data['kode_mapel'] }}&nomor_ujian={{ $data_s['nomor_ujian'] }}">koreksi sekarang</a></th> 
+            @elseif ($data_s["aksi"] == "-") 
+            <th><a type="button" class="btn btn-secondary" disabled>koreksi sekarang</a></th> 
+            @endif
+        </tr>
+        @endforeach
+    </tbody>
+    
+</table>
+@endsection
+
+@section("script")
+<script>
+
+
+new DataTable('#nilai_siswa');
+</script>
+@endsection
