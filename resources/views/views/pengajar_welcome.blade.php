ab2a6f5a239f875049f75b817b5d0e8d9d892369
fadhil riyanto <id.fadhil.riyanto@gmail.com>
2024-06-11 21:04:51 +0700

    add welcome

--- resources/views/views/pengajar_welcome.blade.php
+++ resources/views/views/pengajar_welcome.blade.php
@@ -1,11 +1,21 @@
 @extends("layout._layout_pengajar")
 
 @section("content") 
-hi
+<div class="alert alert-primary mt-1" id="welcome">
+    Selamat datang 
+  </div>
+  
 @endsection
 
 @section("script")
-
-    
+<script>
+    $(document).ready(function() {
+        $.ajax({
+            url: "/api/global/get_me",
+            success: function(data) {
+                $("#welcome").html("selamat datang " + data["data"])
+            }
+        })
+    })
 </script>
 @endsection
